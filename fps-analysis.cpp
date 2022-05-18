a8b056dc277cba21ff0f75f3d20ad550f5ee2318
danelbro <dan.elbro@gmail.com>
2022-05-18 15:55:34 +0100

    Improve FPS analysis tool

--- fps-analysis.cpp
+++ fps-analysis.cpp
@@ -22,14 +22,17 @@ try
 
     auto minmax = std::minmax_element(values.begin(), values.end());
 
-    double average = static_cast<double>(std::accumulate(values.begin(), values.end(), 0))
-        / values.size();
+    double average = static_cast<double>(
+        std::accumulate(values.begin(), values.end(), 0)) / values.size();
     double lowest = values.at(std::distance(values.begin(), minmax.first));
     double highest = values.at(std::distance(values.begin(), minmax.second));
 
-    std::cout << "Average framerate: " << average << '\n'
-              << "Max: " << highest << " (element " << std::distance(values.begin(), minmax.second) << ")\n"
-              << "Min: " << lowest << " (element " << std::distance(values.begin(), minmax.first) << ")\n";
+    std::cout << "Total frames: " << values.size() << '\n'
+              << "Average framerate: " << average << '\n'
+              << "Max: " << highest << " (element "
+              << std::distance(values.begin(), minmax.second) << ")\n"
+              << "Min: " << lowest << " (element "
+              << std::distance(values.begin(), minmax.first) << ")\n";
 
     return 0;
 }
