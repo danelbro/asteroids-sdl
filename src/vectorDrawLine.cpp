c1aebf68c2ba4565c07e533ef87d1858b359da9e
danelbro <dan.elbro@gmail.com>
2022-05-28 06:55:20 +0100

    Implement basic wrapped line drawing
    Not tested yet.

---
+++ src/vectorDrawLine.cpp
@@ -0,0 +1,52 @@
+#include <SDL.h>
+
+void DrawWrapLine(SDL_Renderer *rend, Box screen,
+                  double x1, double y1,
+                  double x2, double y2)
+{
+    double x{ };
+    double y{ };
+    double dy{ y2 - y1 };
+    double dx{ x2 - x1 };
+
+    if (dx == 0) {
+        if (y1 > y2) {
+            y = y2;
+            y2 = y1;
+            y1 = y;
+        }
+        for (y = y1; y <= y2; ++y)
+            SDL_RenderDrawPoint(rend,
+                                wrapCoord(static_cast<int>(x1), screen.width),
+                                wrapCoord(static_cast<int>(y), screen.height));
+    }
+    else {
+        double m{ dy / dx };
+        double c{ y1 - (m * x1) };
+        if (-1 <= m && m <= 1) {
+            if (x1 > x2) {
+                x = x2;
+                x2 = x1;
+                x1 = x;
+            }
+            for (x = x1; x <= x2; ++x) {
+                y = (m * x) + c;
+                SDL_RenderDrawPoint(rend,
+                                    wrapCoord(static_cast<int>(x), screen.width),
+                                    wrapCoord(static_cast<int>(y), screen.height));
+            }
+        }
+        else {
+            if (y1 > y2) {
+                y = y2;
+                y2 = y1;
+                y1 = y;
+            }
+            for (y = y1; y <= y2; ++y) {
+                x = (y - c) / m;
+                SDL_RenderDrawPoint(rend,
+                                    wrapCoord(static_cast<int>(x), screen.width),
+                                    wrapCoord(static_cast<int>(y), screen.height));
+}
+    }
+}
