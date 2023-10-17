af79fb6eff037036c53301dbaf687b99e9be50e0
danelbro <dan.elbro@gmail.com>
2023-10-17 17:39:56 +0100

    Formatting changes

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -1,21 +1,20 @@
 #include "../inc/Asteroid.hpp"
 
-#include <cmath>
-#include <ctime>
-#include <random>
 #include <vector>
 
 #include <SDL.h>
 
+#include "../inc/Colors.hpp"
 #include "../inc/GameWorld.hpp"
 #include "../inc/FlagEnums.hpp"
 #include "../inc/PhysicsComponent.hpp"
 #include "../inc/Vec2d.hpp"
 
-Asteroid::Asteroid(GameWorld *new_gameWorld, Vec2d pos, std::vector<Vec2d> shape, SdlColor color,
-    double scale, PhysicsComponent *new_physicsComponent,
+Asteroid::Asteroid(GameWorld *new_gameWorld, Vec2d pos, std::vector<Vec2d> shape, 
+    SdlColor color, double scale, PhysicsComponent *new_physicsComponent,
     double impulse, double angle, double new_radius)
-    : PhysicsEntity(EntityFlag::ASTEROID, new_gameWorld, pos, shape, color, scale, new_physicsComponent),
+    : PhysicsEntity(EntityFlag::ASTEROID, new_gameWorld, pos, shape, 
+        color, scale, new_physicsComponent),
     radius{ new_radius }
 {
     physicsComponent->setOwner(this);
