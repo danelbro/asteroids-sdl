fbebb684c48cf0ef078247b871e9121b035f98e9
danelbro <dan.elbro@gmail.com>
2024-10-18 10:21:09 +0100

    Make HighScores buildable

--- src/HighScores.cpp
+++ src/HighScores.cpp
@@ -2,8 +2,10 @@
 
 #include "flags.hpp"
 
+#include <array>
 #include <cstdint>
 #include <memory>
+#include <string>
 #include <utl_Box.hpp>
 #include <utl_SDLInterface.hpp>
 #include <utl_Stage.hpp>
@@ -12,7 +14,19 @@
 
 HighScores::HighScores(
     utl::Box& screen, uint32_t windowID, utl::Renderer& renderer,
-    const std::vector<std::unique_ptr<utl::VecGraphPhysEnt>>& physEntities,
-    int score)
+    const std::vector<std::unique_ptr<utl::VecGraphPhysEnt>>&, int)
     : utl::Stage{screen, windowID, renderer, STAGE_MAP[STAGE_ID::HIGH_SCORES]}
 {}
+
+std::string HighScores::handle_input(double, double,
+                                     std::array<bool, utl::KeyFlag::K_TOTAL>&)
+{
+    return STAGE_MAP[STAGE_ID::HIGH_SCORES];
+}
+
+std::string HighScores::update(double, double)
+{
+    return STAGE_MAP[STAGE_ID::HIGH_SCORES];
+}
+
+void HighScores::render(double, double) {}
