14dc01364dfc7176e6425c076b87fbe5df6a95e1
danelbro <dan.elbro@gmail.com>
2024-10-18 10:21:09 +0100

    Make HighScores buildable

--- include/HighScores.hpp
+++ include/HighScores.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <memory>
+#include <string>
 #include <utl_Box.hpp>
 #include <utl_SDLInterface.hpp>
 #include <utl_Stage.hpp>
@@ -14,4 +16,10 @@ public:
         utl::Box& screen, uint32_t windowID, utl::Renderer& renderer,
         const std::vector<std::unique_ptr<utl::VecGraphPhysEnt>>& physEntities,
         int score);
+
+    std::string
+    handle_input(double t, double dt,
+                 std::array<bool, utl::KeyFlag::K_TOTAL>& keyState) override;
+    std::string update(double t, double dt) override;
+    void render(double t, double dt) override;
 };
